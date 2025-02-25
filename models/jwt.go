13aa7c1cf8963a11f08a2da0c72e6aef5a138766
aidanRockie <21993381+rockie2695@users.noreply.github.com>
2025-02-25 19:27:20 +0800

    Update jwt.go

--- models/jwt.go
+++ models/jwt.go
@@ -2,7 +2,7 @@ package models
 
 import "github.com/golang-jwt/jwt/v5"
 
-var JwtKey = []byte("FDr1VjVQiSiybYJrQZNt8Vfd7bFEsKP6vNX1brOSiWl0mAIVCxJiR4/T3zpAlBKc2/9Lw2ac4IwMElGZkssfj3dqwa7CQC7IIB+nVxiM1c9yfowAZw4WQJ86RCUTXaXvRX8JoNYlgXcRrK3BK0E/fKCOY1+izInW3abf0jEeN40HJLkXG6MZnYdhzLnPgLL/TnIFTTAbbItxqWBtkz6FkZTG+dkDSXN7xNUxlg==")
+var JwtKey = []byte("secretkey")
 
 type AuthClaims struct {
 	jwt.RegisteredClaims
